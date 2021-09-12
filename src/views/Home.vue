505ef18572c60d34abfddf3ed3d85ed77a29c6ff
dik <dikki>
2021-09-12 13:17:19 +0700

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <qrocde />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import qrocde from "../components/QrcodeReader";
 
 export default {
   name: "Home",
+
   components: {
-    HelloWorld,
+    qrocde,
   },
 };
 </script>
